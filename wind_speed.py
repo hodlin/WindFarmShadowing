43788a7240a4c7faf791fa6882e70e2c40cbc5b5
hodlin <mr.diomin@gmail.com>
2015-10-20 17:05:41 +0300

    Function that compute wind at specified turbine

---
+++ wind_speed.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from Wind import Wind
+from Turbine import Turbine
+from circle_circle_intersection import circle_intersection_area
+from cone_radius import cone_radius
+from distance_between_circles import distance_between_circles
+
+__author__ = 'dmytro'
+
+
+def wind_speed(w0=Wind(), t1=Turbine(0, 0, 0), t2=Turbine(0, 0, 0)):
+    """
+    Calculate wind speed at second turbine
+    :param t1: first turbine
+    :param t2: second turbine
+    :return:speed in [meters per second]
+    """
+    t_dist = t1.dist(t2)
+    r_radius = t1.d / 2
+    c_radius = cone_radius(t1, t2)
+    c_dist = distance_between_circles(t1, t2)
+
+    in_area = circle_intersection_area(r_radius, c_radius, c_dist)
+
+    w_speed = w0.v0 * (in_area / t1.area)\
+                * (1 - (1 - (t1.w.v0 / (3 * w0.v0)))\
+                * (r_radius / (r_radius + 0.078 * t_dist))**2)\
+                + w0.v0 * ((t1.area - in_area) / t1.area)
+
+    return round(w_speed, 2)
+
+
+if __name__ == "__main__":
+    pass
