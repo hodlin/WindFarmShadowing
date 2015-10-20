e3f95460147c84bcdf71d463b3494d51f58311f5
hodlin <mr.diomin@gmail.com>
2015-10-20 17:01:40 +0300

    Function that computes height of cone

---
+++ cone_height.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from Turbine import Turbine
+from math import cos
+
+__author__ = 'dmytro'
+
+
+def cone_height(t1=Turbine(0, 0, 20), t2=Turbine(0, 0, 20)):
+    """
+    Calculating "height" of wint stream cone from first turbine to the plane of second
+    :param t1: first turbine
+    :param t2: second turbine
+    :return: distance in [meters]
+    """
+    d = t1.dist(t2)
+    direct = t1.direction(t2)
+    theta = t1.w.direction
+
+    if theta == 0:
+        return d * cos(direct)
+
+    if theta > direct:
+        return d * cos(theta - direct)
+
+    if theta < direct:
+        return d / cos(direct - theta)
+
+
+if __name__ == "__main__":
+    pass
