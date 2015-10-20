dde3e595876a2df3559feaae00f1deaae481d444
hodlin <mr.diomin@gmail.com>
2015-10-20 16:57:58 +0300

    Nature - class containing physical constant

---
+++ Nature.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+__author__ = 'dmytro'
+
+
+class Nature(object):
+    """
+    Abstract class that contains main constants
+    """
+    rho = 1.225     # air density in [kilograms / cubic meter]
+    g = 9.81        # gravity acceleration in [meters / square second]
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__author__ = 'dmytro'
